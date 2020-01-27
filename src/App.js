76ece34c42776bb955d7ed4a4f0ae0da211dd7b3
Caesar Ladion <caesar.ladion@gmail.com>
2020-01-26 20:28:04 -0800

    added initial redux store

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import HomePage from "./pages/homepage/homepage.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
